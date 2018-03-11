ab3bd8d251f2352d72e2a85346cfadbc80aeb405
Negiba <negiba@gmail.com>
2018-03-11 17:38:25 +0100

    sredjen edit listener, delete listener, pravilno cuvanje i brisanje elemenata, doradjena paginacija

--- popup/cbmngPopup.js
+++ popup/cbmngPopup.js
@@ -6,7 +6,7 @@ let Popup = (function () {
 
     let getStorageFromBackgroundScript = function (storage) {
         let storageParsed = JSON.parse(storage.getFromStorage());
-        transformStorageToListOfItems(storageParsed);
+        transformStorageToListOfItems(storageParsed); // proveriti da li je potrebna ova metoda
     };
 
     let transformStorageToListOfItems = function (parsed) {
@@ -62,7 +62,7 @@ let Popup = (function () {
     };
 
     let calcPagination = function (elementsPerPage) {
-        let elementsToShow = Popup.getElementsToShow().length;
+        let elementsToShow = getPrivateStorage().length;
         let numsOfPages;
 
         if (elementsToShow <= 10) {
@@ -88,33 +88,31 @@ let Popup = (function () {
     };
 
     let defaultActivePage = function () {
-        let els = document.getElementById("pagination").firstChild;
-        els.setAttribute("class", "active");
+        if (document.getElementById("pagination").firstChild !== null) {
+            let els = document.getElementById("pagination").firstChild;
+            els.setAttribute("class", "active");
+        }
     };
 
     let setActivePage = function (pageNumber) {
         let els = document.getElementById("pagination").childNodes;
         for (let index = 0; index < els.length; index++) {
-            console.log(els[index].value);
             if (els[index].value == pageNumber) {
                 els[index].setAttribute("class", "active");
             } else {
                 els[index].removeAttribute("class");
             }
         }
-        // console.log(els);
     };
 
-    let removeActivePage = function () {
-
-    };
 
     let getActivePage = function () {
-
-    };
-
-    let getLastPage = function () {
-
+        let currentActive = document.getElementsByClassName("active");
+        if (currentActive[0] == undefined) {
+            return 1;
+        } else {
+            return currentActive[0].value;
+        }
     };
 
     return {
@@ -128,18 +126,17 @@ let Popup = (function () {
             browser.runtime.sendMessage({
                 text: item
             });
-
         },
         getElementsToShow: getPrivateStorage,
         showItems: showListOfItems,
         clear: clearTable,
         active: setActivePage,
+        getActive: getActivePage,
     };
 
 })();
 
 Popup.storage();
-Popup.showItems(1);
 
 // Event pagination
 document.getElementById("pagination").addEventListener("click", function (event) {
@@ -155,48 +152,54 @@ document.getElementById("textForCopy").addEventListener("keypress", function (ev
     if (event.keyCode === 13) {
         Popup.addItem("Custom copy" + ">" + document.getElementById("textForCopy").value);
         document.getElementById("textForCopy").value = "";
-        // location.reload();
-        // Popup.getLastPage();
+        location.reload();
     }
 });
 
-// todo - srediti da moze da se odustane od izmene, u ovom trenutuku ukoliko se klikne cancel ili klik misem van okvira popup-a kopija je nepovratno izbrisana
 function editItem() {
-    document.getElementById("textForCopy").value = this.parentNode.firstChild.textContent;
-    let itemsToBackground = [];
-    for (let i = 0; i < document.getElementById("copiedText").rows.length; i++) {
-        if (document.getElementById("copiedText").rows[i].firstChild.textContent !== this.parentNode.firstChild.textContent) {
-            let title = document.getElementById("copiedText").rows[i].firstChild.title;
-            let text = document.getElementById("copiedText").rows[i].firstChild.textContent;
-            let objectToPush = {};
-            objectToPush[title] = text;
-            itemsToBackground.push(objectToPush);
+    let textToEdit = this.parentNode.firstChild.textContent;
+    let title = this.parentNode.firstChild.title;
+    this.parentNode.firstChild.textContent = "";
+    this.parentNode.firstChild.title = "";
+    let edit = document.createElement("input");
+    edit.setAttribute("class", "editElement");
+    edit.value = textToEdit;
+    // this.parentNode.replaceChild(edit, this.parentNode.firstChild);
+    this.parentNode.firstChild.appendChild(edit);
+    edit.focus();
+
+    edit.addEventListener("blur", function () {
+        this.parentNode.firstChild.textContent = textToEdit;
+        edit.remove();
+        location.reload();
+    });
+
+    edit.addEventListener("keypress", function (event) {
+        if (event.keyCode === 13) {
+            let storage = Popup.getElementsToShow();
+            for (let items in storage) {
+                if (items == this.parentNode.parentNode.rowIndex + (Popup.getActive() - 1) * 10) {
+                    for (let item in storage[items]) {
+                        storage[items][item] = edit.value;
+                    }
+                }
+            }
+            Popup.setItems(storage);
+            location.reload();
         }
-    }
-    this.parentNode.remove();
-    Popup.setItems(itemsToBackground);
-    // Popup.showItems(this);
+    });
 }
 
-// todo - ima gresku, svaka strana je zasebna tabela, pa je i rowIndex iz pocetka, ne brise kako treba
+// todo - srediti da na klik obrise kopiju i ali da ne refresuje stranu
 function deleteItem() {
-    console.log("Index " + this.parentNode.rowIndex);
-    let oldDatabase = Popup.getElementsToShow();
-    let itemsToBackground = [];
-    for (let item in oldDatabase) {
-        if (item != this.parentNode.rowIndex) {
-            for (let key in oldDatabase[item]) {
-                let title = key;
-                let text = oldDatabase[item][key];
-                let objectToPush = {};
-                objectToPush[title] = text;
-                itemsToBackground.push(objectToPush);
-            }
+    let storage = Popup.getElementsToShow();
+    for (let items in storage) {
+        if (items == this.parentNode.rowIndex + (Popup.getActive() - 1) * 10) {
+            storage.splice(items, 1);
         }
     }
-    Popup.setItems(itemsToBackground);
-    this.parentNode.remove();
-    // Popup.showItems();
+    Popup.setItems(storage);
+    location.reload();
 }
 
 
